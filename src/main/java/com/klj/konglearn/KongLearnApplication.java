cc2a1d04b39cb9658de8e6dc6e243d61afde4065
Kong LingJun <konglj@jxgtmbjt.com>
2021-07-29 17:39:43 +0800

    init

---
+++ src/main/java/com/klj/konglearn/KongLearnApplication.java
@@ -0,0 +1,13 @@
+package com.klj.konglearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KongLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klj/konglearn/KongLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klj.konglearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
